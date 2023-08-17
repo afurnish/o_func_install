6915f7a690d409fd254f2f79e3004e6de6fd6b70
Aaron Furnish <73606176+afurnish@users.noreply.github.com>
2023-08-17 16:56:32 +0100

    Removed unnecessary waste

--- o_func/utilities/choices.py
+++ o_func/utilities/choices.py
@@ -278,41 +278,42 @@ class DataChoice:
         self.location_of_choices=location_of_choices
         
         
-    # def select(self, path, selection): 
-    #     generic_path_to_file = path + selection
+    def select(self, path, selection, input_message): 
+        #generic_path_to_file = path + selection
         
+        file_length = [str(i) for i in (list(range(1, len(selection) + 1)))]
+        user_input = ''
         
-    #     options = file_split
-     
-    #     user_input = ''
+        for index, item in enumerate(selection):
+            input_message += f'{index+1}) {item}\n'
         
-    #     input_message = "Select a directory by number:\n"
+        input_message += '\nYour choice: '
         
-    #     for index, item in enumerate(options):
-    #         input_message += f'{index+1}) {item}\n'
+        while user_input.lower() not in file_length:
+            print('usr inp', user_input.lower())
+            print('filelength', file_length)
+            user_input = input(input_message)
         
-    #     input_message += 'Your choice: '
-        
-    #     while user_input.lower() not in file_length:
-    #         user_input = input(input_message)
-        
-    #     output = file_split[int(user_input)-1]
+        output = selection[int(user_input)-1]
+        full_path = path + r'/' + output
 
-    #     print('You picked: ' + output + '\n')
-        
-    #     full_path = path + r'/' + output
-    #     return full_path
-        
-    # def dir_select(self, start_path, location_of_choices):
-    #     path = start_path + location_of_choices
-    #     def list_directories(directory_path):
-    #         directories = [item for item in os.listdir(directory_path) if os.path.isdir(os.path.join(directory_path, item))]
-    #         return directories
+        print('You picked: ' + output)
+        print('Full Path: ' + full_path +'\n')
         
         
-            
-    #     self.select(path,list_directories(path))
+        return full_path
         
+        
+    def dir_select(self, start_path, location_of_choices):
+        path = start_path + location_of_choices
+        def list_directories(directory_path):
+            directories = [item for item in os.listdir(directory_path) if os.path.isdir(os.path.join(directory_path, item))]
+            return directories
+        dirs = list_directories(path)
+        
+        input_message = "Select a directory by number:\n"      
+        full_path = self.select(path,dirs,input_message)
+        return full_path
         
     def file_select(self, start_path, location_of_choices,  data_type = 'default'):
         self.data_type = {
@@ -326,40 +327,17 @@ class DataChoice:
         generic_path_to_file = path + self.data_type[data_type] # formally csv etc
         
         files = []
-        file_split = []
-        file_length = []
+        options = []
         # print(generic_path_to_file)
         for j, filename in enumerate(glob.glob(generic_path_to_file)):
             f = winc(filename)
 
             files.append(f)
-            file_split.append(f.split('/')[-1])
-            file_length+=str(j+1)
-        
-        options = file_split
-        user_input = ''
-        
+            options.append(f.split('/')[-1])
         input_message = "Select a file by number:\n"
-        
-        for index, item in enumerate(options):
-            input_message += f'{index+1}) {item}\n'
-        
-        input_message += 'Your choice: '
-        
-        while user_input.lower() not in file_length:
-            user_input = input(input_message)
-        
-        output = file_split[int(user_input)-1]
-
-        print('You picked: ' + output + '\n')
-        
-        full_path = path + r'/' + output
+        full_path = self.select(path, options, input_message)
         return full_path
         
-        
-    #def var_select(self):
-        
-    #def number_of_choice(self):
 #%%      
 if __name__ == '__main__':
     
@@ -369,7 +347,7 @@ if __name__ == '__main__':
     directory_path = r'modelling_DATA/kent_estuary_project/' + \
                         r'land_boundary/analysis/QGIS_shapefiles'
                         
-    directory_path2 = 'modelling_DATA/kent_estuary_project/tidal_boundary/delft_3dfm_inputs/pli_files'
+    directory_path2 = 'modelling_DATA/kent_estuary_project/tidal_boundary/delft_3dfm_inputs'
     
     choice = DataChoice(start_path, directory_path)
     
