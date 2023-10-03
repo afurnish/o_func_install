311d62ef478577e6353e6320b13ae345bef27ef4
Aaron Furnish <afurnish@me.com>
2023-10-03 09:55:59 +0100

    Added grid processing

--- o_func/data_metrics/stats_processing_r_squared_etc.py
+++ o_func/data_metrics/stats_processing_r_squared_etc.py
@@ -7,11 +7,37 @@ Created on Thu Sep 28 09:15:20 2023
 @author: af
 """
 import pandas as pd
+import os
+import xarray as xr
+
+from o_func.utilities.start  import opsys; start_path = opsys()
 
 
 class r_stats:
-    def __init__(self):
-     a = 1
+    def __init__(self, prim_data, ukc3_int_path = 'default'):
+     self.tide_gauge_path = os.path.join(start_path,
+                                         'modelling_DATA',
+                                         'kent_estuary_project',
+                                         'validation',
+                                         'tidal_validation',
+                                         r'1.reformatted')
+     ukc3_path = os.path.join(start_path,
+                  'modelling_DATA',
+                  'kent_estuary_project',
+                  r'6.Final2',
+                  'processed_data',
+                  'ukc4_on_PRIMEA_grid')     
+                  
+                  
+     self.prim_data = prim_data # primea data path             
+                  
+     ukc3_int_path_options = {'default':{'name':'original ukc3 interp dataset',
+                                         'path': ukc3_path}
+                                 }
+     
+     self.ukc3_int_path = ukc3_int_path_options[ukc3_int_path]['path']
+     print(self.ukc3_int_path)
+     
     @staticmethod
     def calculate_hourly_means(df):
         """
@@ -28,7 +54,8 @@ class r_stats:
 
         return hourly_means
 
-    def raw_to_useful(self, prim_tim_df,ukc3_tim_df,var,index_only):
+    def raw_to_useful(self, prim_data,ukc3_data,var,index_only):
+        data_to_populate = []
         '''
         Primary purpose is to allign time dimensions for accurate comparison. Data should be sampled. 
         Depending on the variable you may want to interpolate values between but only for plotting not for 
@@ -48,11 +75,129 @@ class r_stats:
         
         # try not to let hourly stuff get to here on the hour as it messes the whole thing up. 
         '''
+        #path_to_data = path + '/'+ var + '_mean_half_hour_primea_data.pickle'
         
         
+        UKC3_interp = UKC3_interp.z_interpolated_time.values
+        
+        primea_data = PRIMEA_all_data.surface_height.values
+    
+        prim_tim_df2 = prim_tim_df[:-1] # just removes last point in case its erronous
+        primea_data2 = primea_data[:-1,:] # repeated for the data
+        
+        # special dataframe to index everything
+        
+        try:
+            # Unpickle the object from the file
+            if var == 'sh':
+                print('Surface Height Running...')
+                with open(path_to_data, 'rb') as file:
+                    loaded_data = pickle.load(file)
+            elif var == 'sal':
+                print('Salinity Running...')
+                with open(path_to_data, 'rb') as file:
+                    loaded_data = pickle.load(file)
+                
+            with open(path + '/time_data_half_hour_sampled.pickle', 'rb') as file:
+                prim_tim_df4 = pickle.load(file)
+            warnings.filterwarnings("ignore")
+        except Exception as e:
+            print(e)
+            print('\nNo file detected, writing file. ETA 10 minutes')
+
+            new_df = []
+            warnings.filterwarnings("ignore")
+            new_array = []
+            for i,data in enumerate(np.transpose(primea_data2)):
+                del new_df
+                new_df = prim_tim_df2
+                new_df['data'] = data
+                new_df.index = pd.to_datetime(new_df.primea_time)
+                #df = new_df.drop('primea_time', axis=1)
+
+                hourly_means = new_df.resample('1H').mean()
+                hourly_means.index += pd.DateOffset(minutes=30)
+                new_array.append(hourly_means)
+                if i == 0:
+                    prim_tim_df4 = hourly_means.index[24:]
+                if i % 1000 == 0:
+                    print('Run number' + str(i).zfill(3))
+            loaded_data = np.transpose(np.array(new_array)[:,:,0])[24:]
+        
+            warnings.resetwarnings()
+            if var == 'sh':
+                print('Making Surface Height and Running...')
+                with open(path_to_data, 'wb') as file:
+                    pickle.dump(loaded_data, file)
+            elif var == 'sal':
+                print('Making Salinity and Running...')
+                with open(path_to_data, 'wb') as file:
+                    pickle.dump(loaded_data, file)
+            with open(path + '/time_data_half_hour_sampled.pickle', 'wb') as file:
+                pickle.dump(np.transpose(prim_tim_df4), file)
+        warnings.resetwarnings()
+        # # code that goes here sorts the data out into half hour intervals
+        # prim_tim_df3 = prim_tim_df2[3:-4]
+        # prim_tim_df4 = prim_tim_df3[::6] 
+
+        #ukc3_tim_df = ukc3_time[24:]
+        
+        common_idx = ukc3_tim_df[ukc3_tim_df['ukc3_time'].isin(prim_tim_df4)].index
+        if index_only != 'yes':
+            #happy to identify that they are the same. 
+            sliced_ukc3_tim_df = ukc3_tim_df.iloc[common_idx]
+            sliced_ukc3_sh_df = UKC3_interp_z_interp[common_idx,:]
+            sliced_ukc3_tim_df = sliced_ukc3_tim_df
+            sliced_ukc3_sh_df2 = sliced_ukc3_sh_df
+            
+            
+            # sh_primea = primea_data2[3:-4,:]
+            sh_primea2 = loaded_data#sh_primea[::6,:]
+            
+            #calculate_hourly_means(sh_primea)
+            
+            sh_ukc3 = sliced_ukc3_sh_df2
+        
+            return sh_primea2, sh_ukc3, common_idx, UKC3_interp_z_interp
+        else:
+            return common_idx
+        
+    def load_data(self): 
+        globbed = sorted(glob.glob(os.path.join( self.tide_gauge_path, '*.csv')))
+        self.names = []
+        self.df = []
+        for i in globbed:
+            self.names.append(os.path.split(i)[-1][:-4]  )  
+            self.df.append(pd.read_csv(i))
+        print(self.df, self.names)
         
         
+        #In this case UKC4 was interpolated onto PRIMEA grid, load in that data
         
+        #/media/af/PD/modelling_DATA/kent_estuary_project/6.Final2/processed_data/ukc4_on_PRIMEA_grid/sossheig_ogUKC3_.nc
+        self.ukc3_dataset = xr.open_dataset(os.path.join(self.ukc3_int_path, 'UKC3_og_interpolated_PRIMEA_grid_data.nc'))
+        print(self.ukc3_dataset.z_interpolated_time)
+        
+        self.primea_dataset = xr.open_dataset(self.prim_data, chunks={'time':100})
+        
+        
+    def run_stats(self):
+        self.load_data()
+if __name__ == "__main__":
+    from o_func import DataChoice, DirGen
+    import glob
+    
+    #%% Making Directory paths
+    main_path = os.path.join(start_path, r'modelling_DATA','kent_estuary_project',r'6.Final2')
+    make_paths = DirGen(main_path)
+    ### Finishing directory paths
+    
+    dc = DataChoice(os.path.join(main_path,'models'))
+    fn = dc.dir_select()
+    sub_path = make_paths.dir_outputs(os.path.split(fn[0])[-1])
+    lp = glob.glob(os.path.join(sub_path, '*.nc'))[0]
+    rs = r_stats(prim_data = lp)
+    rs.load_data()
         
         
         
--- o_func/utilities/near_neigh.py
+++ o_func/utilities/near_neigh.py
@@ -5,8 +5,72 @@ Created on Thu Sep 28 11:06:47 2023
 
 @author: af
 """
+from sklearn.neighbors import BallTree
 
 import numpy as np
 def data_flattener(x_array,y_array):
     combined_x_y_arrays = np.dstack([y_array.ravel(),x_array.ravel()])[0]
     return combined_x_y_arrays  
+
+def near_neigh(df_loc,df_area,k):
+    '''
+    x and y must be the first 2 columns, other columns do not matter. 
+    Very important that they are both lower case. 
+
+    Parameters
+    ----------
+    df_loc : TYPE
+        DESCRIPTION.
+    df_area : TYPE
+        DESCRIPTION.
+    k : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    distances : TYPE
+        DESCRIPTION.
+    indicies : TYPE
+        DESCRIPTION.
+        
+    Example:
+        df_locs = locations to be found
+              x          y
+        0 -3.016825  53.430732
+        1 -3.642720  54.671460
+        2 -2.931176  54.034552
+        
+        df_area = area of locations to be searched
+                    x          y
+        0     -2.839762  54.204873
+        1     -2.840894  54.205005
+        2     -2.841358  54.205134
+        3     -2.842000  54.205760
+        4     -2.839240  54.204919
+                ...        ...
+        69861 -3.275970  53.487347
+        69862 -3.266266  53.497516
+        69863 -3.269990  53.492205
+        69864 -3.269976  53.489259
+        69865 -3.265208  53.494281
+
+    '''
+    
+    
+    for column in df_area.iloc[:, :2]: 
+        #print(column)           #a
+        rad = np.deg2rad(df_area[column].values)
+        df_area[f'{column}_rad'] = rad
+    for column in df_loc.iloc[:, :2]:
+        #print(column) 
+        rad = np.deg2rad(df_loc[column].values) #b
+        df_loc[f'{column}_rad'] = rad
+
+    # generate haversine ball tree
+    ball = BallTree(df_area[["x_rad", "y_rad"]].values, metric='haversine')
+        
+    # default was 2
+    k = k # number of nearest neigbours to point
+    distances, indicies = ball.query(df_loc[["x_rad", "y_rad"]].values, k = k)
+    
+    return distances, indicies
